ec4d6fd8c152f9a26bf4ae6199a495dccb333956
Hunter McCallum <46570998+huntermccallum@users.noreply.github.com>
2023-04-08 16:44:44 -0600

    Create app.cpp

---
+++ app.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    int num1, num2;
+    
+    // 1. Read two integers from the user
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    // 2. Calculate their sum, difference, product, and quotient
+    int sum = num1 + num2;
+    int difference = num1 - num2;
+    int product = num1 * num2;
+    float quotient = static_cast<float>(num1) / num2;
+
+    // 3. Print the results
+    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "Difference: " << difference << std::endl;
+    std::cout << "Product: " << product << std::endl;
+    std::cout << "Quotient: " << quotient << std::endl;
+
+    return 0;
+}
